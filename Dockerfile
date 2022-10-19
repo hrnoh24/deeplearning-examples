1171233729c0572a17022b42a82cb8a70a3caf99
hrnoh24 <hrnoh24@gmail.com>
2022-10-19 22:58:07 +0900

    Add docker

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM nvcr.io/nvidia/pytorch:22.05-py3
+
+WORKDIR /workspace
+
+RUN apt-get update
+ADD requirements.txt .
+RUN pip install -r requirements.txt
+
+EXPOSE 8888
+EXPOSE 6006
+
+CMD ["bash"]
---
+++ scripts/docker_interactive.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DATA_DIR="$1"
+
+ARGS=""
+ARGS+=" -it"
+ARGS+=" --rm"
+ARGS+=" --gpus all --ipc host"
+ARGS+=" -p 6006:6006"
+ARGS+=" -p 8888:8888"
+ARGS+=" -v ${PWD}:/workspace"
+[ "${DATA_DIR}" != "" ] && ARGS+=" -v ${DATA_DIR}:/data"
+
+docker run ${ARGS} muse
